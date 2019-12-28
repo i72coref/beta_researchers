3e0dc5cff57fb6796727ec93a738d25c0f54cb4a
PC <PC@DESKTOP-NLPG4VA>
2019-12-28 18:13:01 +0100

    angel

---
+++ src/es/uco/pw/data/DAO/b_avanzadaDao.java
@@ -0,0 +1,7 @@
+package es.uco.pw.data.DAO;
+
+import java.sql.*;
+
+public class b_avanzadaDao {
+
+}
---
+++ src/es/uco/pw/display/beans/b_avanzadaBean.java
@@ -0,0 +1,8 @@
+package es.uco.pw.display.beans;
+
+import java.io.Serializable;
+import java.io.SerializablePermission;
+
+public class b_avanzadaBean extends SerializablePermission implements Serializable {
+
+}
